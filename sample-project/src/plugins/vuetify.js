0f4caea4d80a023f24e57d380d46d9e4dc492e68
Ali Akıncı <tugceorcn@hotmail.com>
2022-08-14 17:51:00 +0300

    axios plugin,  sass setup done

--- sample-project/src/plugins/vuetify.js
+++ sample-project/src/plugins/vuetify.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
 
+import "vuetify/dist/vuetify.min.css";
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
     iconfont: "fa",
   },
+  theme: { light: true },
 });
